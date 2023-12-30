7411e25a015f375918cf22ae22e9c90d9474fe58
Kolakanmi Apanisile <apanisilekolakanmi@gmail.com>
2023-12-29 21:43:39 -0400

    add more methods

--- matrix.hpp
+++ matrix.hpp
@@ -9,13 +9,12 @@
 #include <initializer_list>
 #include <istream>
 #include <sstream>
+#include <random>
 
 using element = double;
 
 class Matrix {
 public:
-    size_t rows;
-    size_t cols;
 
     Matrix(size_t rows, size_t cols);
     Matrix(size_t rows, size_t cols, element *items);
@@ -39,41 +38,51 @@ public:
 
     // sum matrix
     Matrix operator+(const Matrix &m) const;
-
     [[nodiscard]] Matrix add(const Matrix &m) const;
-
     Matrix operator+(const Matrix &m);
-
     Matrix add(const Matrix &m);
 
     // subtract matrix
     Matrix operator-(const Matrix &m) const;
-
     [[nodiscard]] Matrix sub(const Matrix &m) const;
-
     Matrix operator-(const Matrix &m);
-
     Matrix sub(const Matrix &m);
 
     // multiply matrix
     Matrix operator*(const Matrix &m) const;
-
     [[nodiscard]] Matrix mul(const Matrix &m) const;
-
-
 
     element at(size_t row, size_t col) const;
 
     void set_value(size_t row, size_t col, element value);
+
+    [[nodiscard]] Matrix sub_matrix(size_t start_row, size_t start_col, size_t end_row, size_t end_col) const;
+    [[nodiscard]] Matrix sub_matrix(size_t start_row, size_t end_row, size_t num_cols) const;
+
+    [[nodiscard]] size_t get_rows() const;
+    [[nodiscard]] size_t get_cols() const;
+
+    [[nodiscard]] size_t size() const;
+
+    void print(const char *name = nullptr) const;
+
+    Matrix &randomize(element min, element max);
 
     // destructor
     ~Matrix() {
         delete[] items;
     }
 private:
+    size_t rows;
+    size_t cols;
+
     size_t stride;
     element *items;
+
+    Matrix(size_t row, size_t col, size_t stride);
 };
+
+void print(const Matrix &m, const char *name = nullptr);
 
 Matrix::Matrix(size_t rows, size_t cols) {
     this->rows = rows;
@@ -149,8 +158,8 @@ Matrix::Matrix(std::istream &is) {
 
     // copy the elements from the vector of vector of elements to the matrix
     size_t i = 0;
-    for (auto &line : lines) {
-        for (auto &e : line) {
+    for (auto &l : lines) {
+        for (auto &e : l) {
             this->items[i] = e;
             ++i;
         }
@@ -299,6 +308,92 @@ element Matrix::at(size_t row, size_t col) const {
 void Matrix::set_value(size_t row, size_t col, element value) {
     this->items[row * this->stride + col] = value;
 }
+
+Matrix::Matrix(size_t row, size_t col, size_t stride) {
+    this->rows = row;
+    this->cols = col;
+    this->stride = stride;
+    this->items = new element[row * col];
+}
+
+Matrix Matrix::sub_matrix(size_t start_row, size_t start_col, size_t end_row, size_t end_col) const {
+    size_t r = end_row - start_row + 1;
+    size_t c = end_col - start_col + 1;
+    size_t s = this->stride;
+
+//    return this->sub_matrix(start_row, start_col, end_row, end_col);
+//
+//    auto i = this->items + (start_row * s + start_col);
+
+    Matrix result = Matrix(r, c, s);
+
+    for (size_t i = start_row; i < result.rows; ++i) {
+        for (size_t j = start_col; j < result.cols; ++j) {
+            result.set_value(i - start_row, j - start_col, this->at(i, j));
+        }
+    }
+
+    return result;
+}
+
+Matrix Matrix::sub_matrix(size_t start_row, size_t end_row, size_t num_cols) const {
+    size_t r = end_row - start_row + 1;
+    size_t c = num_cols;
+    size_t s = this->stride;
+
+    Matrix result = Matrix(r, c, s);
+
+    for (size_t i = start_row; i < result.rows; ++i) {
+        for (size_t j = 0; j < result.cols; ++j) {
+            result.set_value(i - start_row, j, this->at(i, j));
+        }
+    }
+
+    return result;
+}
+
+size_t Matrix::get_rows() const {
+    return this->rows;
+}
+
+size_t Matrix::get_cols() const {
+    return this->cols;
+}
+
+size_t Matrix::size() const {
+    return this->rows * this->cols;
+}
+
+void Matrix::print(const char *name) const {
+    ::print(*this, name);
+}
+
+Matrix &Matrix::randomize(element min, element max) {
+    std::random_device rd;
+    std::default_random_engine gen(rd());
+    std::uniform_real_distribution<float> dis(0, 1);
+
+    for (size_t i = 0; i < this->rows * this->cols; ++i) {
+        this->items[i] = dis(gen) * (max - min) + min;
+    }
+}
+
+void print(const Matrix &m, const char *name) {
+    if (name != nullptr) {
+        printf("%s = [\n", name);
+    } else {
+        printf("[\n");
+    }
+
+    for (size_t i = 0; i < m.get_rows(); ++i) {
+        for (size_t j = 0; j < m.get_cols(); ++j) {
+            printf("    %f ", m.at(i, j));
+        }
+        printf("\n");
+    }
+    printf("]\n");
+}
+
 
 
 #endif //KOLAKANMI_MATRIX_HPP
